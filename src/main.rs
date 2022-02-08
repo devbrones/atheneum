25f850f78b4283ec6468923684ce574e505a17c9
devbrones <jonas.cronholm@protonmail.com>
2022-02-08 10:12:28 +0100

    fuck

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::io;
 use tui::Terminal;
 use tui::backend::CrosstermBackend;
+use curl::easy::Easy;
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    Ok(())
+    Ok(());
+    let mut woltest = Easy::new();
+    woltest.url("https://scrap.madiator.com/api/is_online?v=2").unwrap();
+    let mut woltest_trans = woltest.transfer();
+    woltest_trans.
+    
+    
 }
